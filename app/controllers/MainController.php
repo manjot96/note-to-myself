5d00cd0d906313b8e8c8211d27c514b17b02cfc9
Manjot Khalsa <manjotkhalsa.96@gmail.com>
2016-03-08 00:16:33 -0800

    Images added

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -52,15 +52,25 @@ class MainController extends \BaseController {
             $res = User::select('_ID')->where('emailaddress', $_SESSION["email"])->first();
             //check if $res is empty here
             $_SESSION["_ID"] = $res["_ID"];
+            
             $res = Note::select('note')->where('_ID', $_SESSION["_ID"])->get()->toArray();
             $_SESSION["notes"] = (!empty($res)) ? $res[0]["note"] : "";
+            
             $res = TBD::select('text')->where('_ID', $_SESSION["_ID"])->get()->toArray();
             $_SESSION["tbd"] = (!empty($res)) ? $res[0]["text"] : "";
+            
             $res = Website::select('urls')->where('_ID', $_SESSION["_ID"])->get()->toArray();
             $_SESSION["urls"] = array();
             foreach($res as $url) {
                 array_push($_SESSION["urls"], $url["urls"]);
             }
+            
+            $res = Image::select('image')->where('_ID', $_SESSION["_ID"])->get()->toArray();
+            $_SESSION["images"] = array();
+            foreach($res as $image) {
+                array_push($_SESSION["images"], $image["image"]);
+            }
+            
 			return View::make('home');
 		} else{
 			return Redirect::back()->withInput();
@@ -91,10 +101,14 @@ class MainController extends \BaseController {
         Note::where('_ID', $_SESSION["_ID"])
         ->update(array('note' => $_POST["notes"]));
         $_SESSION["notes"] = $_POST["notes"];
+        
         TBD::where('_ID', $_SESSION["_ID"])
         ->update(array('text' => $_POST["tbd"]));
         $_SESSION["tbd"] = $_POST["tbd"];
+        
+        //Deleting all the websites from our database first so we can repopulate it afterwards;
         DB::delete('delete from Websites where _ID="'.$_SESSION["_ID"].'"');
+        
         $_SESSION["urls"] = array();
         foreach($_POST["websites"] as $buffalo) {
             if(!empty($buffalo)) {
@@ -102,6 +116,20 @@ class MainController extends \BaseController {
                 Website::insert(array('_id' => $_SESSION["_ID"],
                 'urls' => $buffalo));
             }
+        }
+        
+        if($_FILES['image']['error'] === UPLOAD_ERR_OK){
+            if($_FILES['image']['type'] == "image/jpeg" || $_FILES['image']['type'] == "image/jpg"
+                || $_FILES['image']['type'] == "image/gif" ) {
+                $image = file_get_contents($_FILES['image']['tmp_name']);
+                Image::insert(array('_id' => $_SESSION["_ID"],
+                'image' => $image));
+            }
+        }
+        $res = Image::select('image')->where('_ID', $_SESSION["_ID"])->get()->toArray();
+        $_SESSION["images"] = array();
+        foreach($res as $image) {
+            array_push($_SESSION["images"], $image["image"]);
         }
         return View::make('home');
 	}
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -4,8 +4,8 @@
 //add $hell into the sessions when user is logged in;
 if(!isset($_SESSION["email"]))
     return View::make('login');
-
-echo $_SESSION["notes"]."<br>";
+foreach($_SESSION["images"] as $img)
+    $image = $img;
 ?>
 
 <!doctype html>
@@ -17,7 +17,7 @@ echo $_SESSION["notes"]."<br>";
 </head>
 <body>
     <div id="wrapper">
-    <form action="update" method="post">
+    <form action="update" method="post"  enctype="multipart/form-data">
         <h2 id="header">{{$_SESSION["email"]}} - <span><a href="/logout">Log out</a></span></h2>
         <div id="section1">
 
@@ -40,7 +40,22 @@ echo $_SESSION["notes"]."<br>";
         <div id="section2">
             <div id="column3">
                 <h2>Images</h2><h3>Click for full screen</h3>
-                <input type="file" name="image" /> <br/> <br/>
+                {{Form::file('image')}}
+                <br>
+                <br>
+                <div>
+                    <?php 
+                    foreach($_SESSION["images"] as $img) {
+                        echo '<div>
+                        <a href="data:image/jpeg;base64,'.base64_encode($img).'">
+                        <img src="data:image/jpeg;base64,'.base64_encode($img).'"/>
+                        </a>
+                        <input type=\'checkbox\' name=\'delete[]\' value=\'165\' /> 
+                        <label for=\'delete[]\'>delete</label><br /><br />
+                        </div>';
+                    } 
+                    ?>
+                </div>
             </div>
             <div id="column4">
                 <h2>TBD</h2>
