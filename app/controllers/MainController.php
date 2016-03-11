d2fe7ca9ad1b49624f2c590b8adc9937283f90ae
Manjot Khalsa <manjotkhalsa.96@gmail.com>
2016-03-10 20:18:56 -0800

    Updated session_destroy/logout

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -220,9 +220,19 @@ class MainController extends \BaseController {
 	public function logout()
 	{
 		if (isset($_SESSION["email"])) {
-            //grab more code for session_destroy from php
+            $email = $_SESSION["email"];
+            $_SESSION = array();
+            if (ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000,
+                    $params["path"], $params["domain"],
+                    $params["secure"], $params["httponly"]
+                );
+            }
             session_destroy();
-        } return View::make('logout');
+            return View::make('logout')->with('email', $email);	
+        } 
+        return View::make('logout');
 		
 	}
 
--- app/views/logout.blade.php
+++ app/views/logout.blade.php
@@ -6,14 +6,11 @@
     <title>Note to Myself - Log out</title>
 </head>
 <body>
-	<?php 
-	if((isset($_SESSION["email"]))) {
-		echo "<h1>" . $_SESSION['email'] . " is now logged out. Thank you</h1>";
-		echo "<p><a href=\"/login\">Log in</a> again.</p>";
-	} else
-		echo "<p><a href=\"/login\">Log in</a> or <a href=\"/register\">register</a> before logging out.</p>";
-	?>
-    
-     
+	@if ((isset($_SESSION["email"])) || isset($email))
+		<h1> {{(isset($email)) ? $email : $_SESSION['email']}} is now logged out. Thank you</h1>
+		<p><a href="/login">Log in</a> again.</p>
+	@else
+		<p><a href="/login">Log in</a> or <a href="/register">register</a> before logging out.</p>
+	@endif
 </body>
 </html>
